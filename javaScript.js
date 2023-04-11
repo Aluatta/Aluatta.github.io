145a57af74a8e2203bb88d32c2e49468687d6a37
Aluatta <113020742+Aluatta@users.noreply.github.com>
2023-04-11 08:32:36 +0200

    javascript site

---
+++ javaScript.js
@@ -0,0 +1,17 @@
+function fun1(){console.log('hello');
+}
+
+
+function fun2(a,b){return(a+b);}
+const result= fun2(1,3);  
+console.log(result);
+
+const fun3 = function(a,b) { return a+b;};
+fun3=(1,3)
+
+
+
+const fun4 = function fun5(a,b){console.log()
+
+}
+
