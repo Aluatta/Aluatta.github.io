3a04dfd17c163f430c440245ab375da7a1e649bf
Aluatta <113020742+Aluatta@users.noreply.github.com>
2023-04-13 09:13:47 +0200

    hamburger i gang

--- javaScript.js
+++ javaScript.js
@@ -15,3 +15,33 @@ const fun4 = function fun5(a,b){console.log()
 
 }
 
+
+/**  - - - - - - - - - - - - - - - - - - -- -   */
+// Selecting elements in the HTML document:
+const hamburger = document.querySelector('.hamburger');
+
+
+
+//console.log(hamburger); // to see what is in the variable: hamburger
+const navMenu = document.querySelector('.nav-menu');
+
+const navLink = document.querySelectorAll('.nav-link');
+console.log(navLink);
+
+// Listening for a click event on the hamburger button:
+hamburger.addEventListener('click', mobileMenu);
+
+// Eventlistener for all navLinks:
+navLink.forEach(n => n.addEventListener('click',closeMenu));
+
+// When the hamburger button is clicked
+function mobileMenu() {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+}
+
+// When a menu-item is clicked - the active classes are removed from hamburger and navMenu
+function closeMenu() {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+}
